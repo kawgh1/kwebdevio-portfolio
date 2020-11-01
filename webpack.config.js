62c30ded9d7700ff98eb5b73e2ed2972c74200e6
thrwwy216 <thrwwy216@tutanota.com>
2020-11-01 17:36:23 -0600

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,12 @@
 const path = require('path')
 
 module.exports = {
-    devServer: {
-        contentBase: path.resolve(__dirname, './public'),
-        historyApiFallback: true,
-    },
-    entry: path.resolve(__dirname, './src/index.js'),
-    resolve: {
-        extensions: ['*', '.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-
-            },
-            {
-                test: /\.(scss)$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-
-                    {
-                        loader: 'sass-loader',
-                    },
-                ],
-                exclude: /node_modules/,
-            },
-        ],
-    },
+    entry: [
+        './src/index.js'
+    ],
     output: {
-        filename: 'bundle.js',
-    },
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    }
 }
